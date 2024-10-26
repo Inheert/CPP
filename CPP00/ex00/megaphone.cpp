102a30877678402e402a6890597b055dcfdd548a
Inheert <theoclaereboudt@gmail.com>
2024-10-26 15:41:14 +0200

    W

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,13 +6,22 @@
 /*   By: tclaereb <tclaereb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 08:42:22 by tclaereb          #+#    #+#             */
-/*   Updated: 2024/10/26 14:29:19 by tclaereb         ###   ########.fr       */
+/*   Updated: 2024/10/26 15:40:47 by tclaereb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Megaphone/Megaphone.hpp"
+#include <iostream>
 
 int	main(int argc, char **argv)
 {
-	Megaphone	megaphone(argc, argv);
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < argc; i++) {
+		for (int j = 0; argv[i][j]; j++)
+			std::cout << static_cast<unsigned char>(toupper((argv[i][j])));
+	}
+	std::cout << std::endl;
 }
