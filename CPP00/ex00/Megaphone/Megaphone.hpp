13a22a096e2f794e556d373826bfd384e710e509
Inheert <theoclaereboudt@gmail.com>
2024-10-26 15:35:23 +0200

    f

--- CPP00/ex00/Megaphone/Megaphone.hpp
+++ CPP00/ex00/Megaphone/Megaphone.hpp
@@ -6,7 +6,7 @@
 /*   By: tclaereb <tclaereb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 14:26:32 by tclaereb          #+#    #+#             */
-/*   Updated: 2024/10/26 15:32:53 by tclaereb         ###   ########.fr       */
+/*   Updated: 2024/10/26 15:35:18 by tclaereb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,5 @@ class	Megaphone {
 
 	public:
 		Megaphone(int argc, char **argv);
+
 };
